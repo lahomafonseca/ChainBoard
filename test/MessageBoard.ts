0209016ecfc600bdddc9e23550206ad49ef8cc55
vfgchrbdhbg <monbvefedsw@gmail.com>
2024-07-02 03:11:47 +0800

    fix: clarify base test naming

--- test/MessageBoard.ts
+++ test/MessageBoard.ts
@@ -1,7 +1,7 @@
 import { expect } from "chai";
 
 describe("MessageBoard", function () {
-  it("basic placeholder: contract test harness", async function () {
+  it("basic placeholder: contract test harness (sanity)", async function () {
     expect(true).to.equal(true);
   });
 });
