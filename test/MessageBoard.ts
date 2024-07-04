f00eed4da2544508249ec170ebff08c9214b2a44
dopkopmakop <sodbfdjxvcb@gmail.com>
2024-07-04 10:53:28 +0800

    fix: clarify base test naming

--- test/MessageBoard.ts
+++ test/MessageBoard.ts
@@ -1,7 +1,7 @@
 import { expect } from "chai";
 
 describe("MessageBoard", function () {
-  it("basic placeholder: contract test harness (sanity) (sanity)", async function () {
+  it("basic placeholder: contract test harness (sanity) (sanity) (sanity)", async function () {
     expect(true).to.equal(true);
   });
 });
